c627a5e183586c11aabdb519e173e4eb581d0fad
Sri Harish B <150308442+SriHarishb@users.noreply.github.com>
2024-09-20 19:23:51 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import MainScreen from './components/MainScreen';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
+import ResetPage from './components/ResetPage';
+import EnterOTP from './components/EnterOTP';
+
 import './index.css';
 import './LoginPage.css';
 
@@ -10,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<MainScreen />} />
         <Route path="/LoginPage" element={<LoginPage />} />
+        <Route path='/ResetPage' element={<ResetPage />} />
+        <Route path='/EnterOTP' element={<EnterOTP />} />
       </Routes>
     </Router>
   );
